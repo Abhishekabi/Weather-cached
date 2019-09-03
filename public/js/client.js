3c438446bc125c088ace4c3c8f0defd1a1a1a1d4
Abhishekabi <abimano99@gmail.com>
2019-09-03 14:36:34 +0530

    code review changes

--- public/js/client.js
+++ public/js/client.js
@@ -1,22 +1,52 @@
 var inputField = $("#search");
 var submit = $("#weatherbutton");
 
+// UI event listeners
 submit.on("click", () => {
-  var city = inputField.val();
-  inputField.val("");
-  fetchWeather(city.trim());
+  eventHandler.handleClick();
 });
 
 inputField.on("keypress", event => {
   var keycode = event.keyCode ? event.keyCode : event.which;
   if (keycode == "13") {
-    var city = inputField.val();
-    fetchWeather(city.trim());
-    inputField.val("");
+    eventHandler.handleClick();
     return false;
   }
 });
 
+// To handle events
+var eventHandler = {
+  handleClick: function() {
+    var city = inputField.val();
+    inputField.val("");
+    fetchWeather(city.trim());
+  }
+};
+
+// Server calls
+var weatherAPI = {
+  getWeather: function(city) {
+    $.ajax(`/weather/${city}`, {
+      dataType: "json",
+      success: function(data, status, xhr) {
+        if (data.message) setErrorMessage(data.message);
+        else setData(data);
+      },
+      error: function(jqXhr, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+};
+
+// Healper functions
+var fetchWeather = function(city) {
+  if (city.length > 0) {
+    weatherAPI.getWeather(city);
+  }
+};
+
+// UI implementation
 var setData = function({ city, temp, humid, desc }) {
   city = city.charAt(0).toUpperCase() + city.slice(1);
   $(".advance-search > .row").removeClass("dN");
@@ -33,19 +63,4 @@ var setErrorMessage = function(message) {
   $(".advance-search > .desc")
     .text(`Oops! ${message}`)
     .css("color", "rgba(255, 82, 82, 1.0)");
-};
-
-var fetchWeather = function(city) {
-  if (city.length > 0) {
-    $.ajax(`/weather/${city}`, {
-      dataType: "json",
-      success: function(data, status, xhr) {
-        if (data.message) setErrorMessage(data.message);
-        else setData(data);
-      },
-      error: function(jqXhr, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
-    });
-  }
 };
